f8444327b1db4614ab8cbdc7f5ee984c5f00f30a
Josias Matos <josiasmatosb@gmail.com>
2025-09-07 12:27:47 -0400

    added new component + new styles to the website

--- src/app/Components/home/home.ts
+++ src/app/Components/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Experience } from '../experience/experience';
+import { Education } from '../education/education';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Experience, Education],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './Components/header/header';
 import { Footer } from './Components/footer/footer';
+import { Experience } from './Components/experience/experience';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Experience],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
